225e2f3e4ed6ec7f9ad0c9959dd44156b01aa125
Jerome-LorD <jerome-divry@laposte.net>
2021-07-05 10:59:11 +0200

    first tests

--- purapps/purbeurre/tests.py
+++ purapps/purbeurre/tests.py
@@ -1,15 +1,59 @@
+"""Tests purbeurre models module."""
+from purapps.purbeurre.models import Category, Product, Nutriscore
 from django.test import TestCase
 
-# from purbeurre.models import Nutriscore
+# from purbeurre.models import Product
 
-# class AnimalTestCase(TestCase):
-#     def setUp(self):
-#         Nutriscore.objects.create(name="café", sound="roar")
-#         Nutriscore.objects.create(name="chocolat", sound="meow")
 
-#     def test_animals_can_speak(self):
-#         """Animals that can speak are correctly identified"""
-#         lion = Nutriscore.objects.get(name="lion")
-#         cat = Nutriscore.objects.get(name="cat")
-#         self.assertEqual(lion.speak(), 'The lion says "roar"')
-#         self.assertEqual(cat.speak(), 'The cat says "meow"')
+class ProductTestCase(TestCase):
+    """ProductTestCase class."""
+
+    def setUp(self):
+        """Make Setup."""
+        Product.objects.create(
+            name="café",
+            brand="bla",
+            stores="bli",
+            image="blo",
+            url="http://chocolate.com",
+        )
+        Product.objects.create(
+            name="chocolat",
+            brand="bla",
+            stores="bli",
+            image="blo",
+            url="http://coffee.com",
+        )
+
+    def test_create_product(self):
+        """test created products."""
+        coffee = Product.objects.get(name="café")
+        chocolate = Product.objects.get(name="chocolat")
+        self.assertEqual(coffee.name, "café")
+        self.assertEqual(chocolate.name, "chocolat")
+
+
+class CategoryTestCase(TestCase):
+    """CategoryTestCase class."""
+
+    def setUp(self):
+        """Make Setup."""
+        Category.objects.create(name="chocolat")
+
+    def test_create_category(self):
+        """Test created categories."""
+        cat_chocolate = Category.objects.get(name="chocolat")
+        self.assertEqual(cat_chocolate.name, "chocolat")
+
+
+class NutriscoreTestCase(TestCase):
+    """NutriscoreTestCase class."""
+
+    def setUp(self):
+        """Make Setup."""
+        Nutriscore.objects.create(type="a")
+
+    def test_create_Nutriscore(self):
+        """Test created nutriscore."""
+        nutriscore = Nutriscore.objects.get(pk=1)
+        self.assertEqual(nutriscore.type, "a")
