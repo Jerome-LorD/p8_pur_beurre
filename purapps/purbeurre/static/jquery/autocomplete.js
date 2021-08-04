6be0e8a505a3797425a06d36fe00d3ee14556fc2
Jerome-LorD <jerome-divry@laposte.net>
2021-08-04 10:38:55 +0200

    adding js scripts under jquery folder

---
+++ purapps/purbeurre/static/jquery/autocomplete.js
@@ -0,0 +1,24 @@
+$(function () {
+    $("#products").autocomplete({
+        minLength: 3,
+        source: "autocomplete/",
+        focus: function (event, ui) {
+            $("#products").val(ui.item.name);
+            return false;
+        },
+        select: function (event, ui) {
+            $("#products").val(ui.item.name);
+            $("#products-id").val(ui.item.value);
+            $("#products-icon").attr("src", "{% static '/' %}" + ui.item.icon);
+            $(this).closest("form").submit();
+
+            return false;
+        }
+    })
+        .autocomplete("instance")._renderItem = function (ul, item) {
+            return $("<li>")
+                .append("<div>" + item.name + "</div>")
+                .appendTo(ul);
+        };
+
+});
---
+++ purapps/purbeurre/static/jquery/get_substitutes.js
@@ -0,0 +1,20 @@
+var get_substitutes = function () {
+    $('#save_btn').click(function (e) {
+        var selected_products = [];
+        $("input:checkbox:checked").each(function () {
+            selected_products.push($(this).val());
+        });
+
+        $.ajax({
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            type: 'POST',
+            url: '/ajax/',
+            data: JSON.stringify({ "products": selected_products, "user_id": '{{user.id}}', "origin_product": '{{origin_prod_name}}' }),
+            csrfmiddlewaretoken: '{{ csrf_token }}',
+
+        });
+        e.stopImmediatePropagation();
+        /*return;*/
+    });
+};
