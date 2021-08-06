2ae7b70261a5c8ec9913ea82fa091a2be29c8c2a
Mikael Briolet <mbriolet.ma@gmail.com>
2021-08-06 13:32:40 +0200

    Revue du code
    Le formulaire de recherche devrait avoir sa propre vue pour éviter de le dupliquer X fois à chaque vue. ;)

--- purapps/purbeurre/views.py
+++ purapps/purbeurre/views.py
@@ -12,23 +12,23 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
     """Index."""
-    if request.method == "POST":
-        search_form = SearchProduct(request.POST or None)
+    if request.method == "POST":    # le formulaire ne devrait pas se résoudre ici
+        search_form = SearchProduct(request.POST or None)  # pas sur que le or None soit necessaire
         if search_form.is_valid():
-            search_form = SearchProduct()
-            product_name = request.POST.get("product_name")
+            search_form = SearchProduct()  # à priori inutile ici
+            product_name = request.POST.get("product_name")  # tu devrais pouvoir le récupérer depuis le formulaire directement
 
             return redirect("results", product_name=product_name)
     else:
         search_form = SearchProduct()
 
     context = {"search_form": search_form}
-    return render(request, "pages/home.html", context and locals())
+    return render(request, "pages/home.html", context and locals())  # pourquoi "and locals()" ?
 
 
 def results(request, product_name):
-    """Results."""
-    if request.method == "POST":
+    """Results."""  # results de quoi ? ;)
+    if request.method == "POST":   # le formulaire ne devrait pas se résoudre ici
         search_form = SearchProduct(request.POST or None)
         if search_form.is_valid():
             search_form = SearchProduct()
@@ -36,16 +36,23 @@ def results(request, product_name):
 
             return redirect("results", product_name=product_name)
 
-    search_form = SearchProduct()
+    search_form = SearchProduct()  # si tu le créé ici, ne le créé pas plus bas ;)
+    
     result = Product.objects.filter(name__iregex=r"^%s$" % product_name)
     if result:
+        
+        # préfère plutôt:
+        product = result.first()
+        # puis product.name, produuct.nutriscore, etc.
+        
+        # préfère aussi nommer tes variables sans abréviation ;)
 
         origin_prod_name = result[0].name
         origin_nutriscore = result[0].nutriscore.type.capitalize()
         origin_category = result[0].categories.values("id")
         origin_image = result[0].image
 
-        substit = Product.find_substitute(result[0].id)
+        substit = Product.find_substitute(result[0].id)  # avec une méthode d'instance, ce sera "product.find_substitute()" tout simplement
         page = request.GET.get("page", 1)
         paginator = Paginator(substit, 8)
         try:
@@ -61,6 +68,7 @@ def results(request, product_name):
             {
                 "search_form": search_form,
                 "substit": substit,
+                # préfère envoyer directement le produit plutot que ses attributs un à un (plus simple et lisible)
                 "origin_prod_name": origin_prod_name,
                 "origin_image": origin_image,
                 "origin_nutriscore": origin_nutriscore,
@@ -77,7 +85,8 @@ def results(request, product_name):
             page_no_result = paginator.page(1)
         except EmptyPage:
             page_no_result = paginator.page(paginator.num_pages)
-        search_form = SearchProduct()
+ 
+        search_form = SearchProduct()  # existe déjà
         return render(
             request,
             "pages/results.html",
@@ -97,8 +106,8 @@ def err_404(request, exception=None):
 
 def product_details(request, product_name):
     """Product details."""
-    if request.method == "POST":
-        search_form = SearchProduct(request.POST or None)
+    if request.method == "POST":   # le formulaire ne devrait pas se résoudre ici
+        search_form = SearchProduct(request.POST or None)  # pourquoi "or None" à chaque fois ?
         if search_form.is_valid():
             search_form = SearchProduct()
             product_name = request.POST.get("product_name")
@@ -106,6 +115,7 @@ def product_details(request, product_name):
             return redirect("results", product_name=product_name)
 
     search_form = SearchProduct()
+    # dans l'idéal, appel une méthode du produit pour récupérer tous ces éléments - on est dans de la logique métier ici ;)
     try:
         product = Product.objects.get(name=product_name)
         fat = product.nutriments["fat_100g"]
@@ -151,19 +161,21 @@ def autocomplete(request):
     return HttpResponse(data, mimetype)
 
 
-@csrf_exempt
-def ajax(request):
-    """Ajax post method."""
+@csrf_exempt  # tu peux implémenter le csrf avec l'ajax, mais ça demande un peu de documentation (django en parle dans sa doc)
+def ajax(request):  # qui fait quoi ?
+    """Ajax post method."""  # qui fait quoi ?
     if request.is_ajax():
-        body_unicode = request.body.decode("utf-8")
+        body_unicode = request.body.decode("utf-8")  # ne décode pas le body comme ça pour récupérer le json, mais utilise seulement la lib json
+        # peut être mettre le content-type dans fectch (application/json) pour éviter ce genre de traitement ;) je te laisse te renseigner.
         body = json.loads(body_unicode)
         products = body["products"]
-        user_id = body["user_id"]
-        origin_product = body["origin_product"]
+        user_id = body["user_id"]  # pas besoin de passer le user_id puisque tu l'as avec request ;)
+        origin_product = body["origin_product"]  # préfère "product" ou "base_product", plus simple. Si tu n'as que le produit et ses substituts, alors "product" et "substitutes" suffit.
 
-        origin_product = origin_product.replace("&amp;", "&")
+        origin_product = origin_product.replace("&amp;", "&") # pas besoin de ça normalement
         origin_product = Product.objects.get(name=origin_product)
 
+        # je ne comprend pas cette partie
         for item_id in products:
             if Substitutes.objects.filter(
                 product_id=item_id,
@@ -186,7 +198,7 @@ def ajax(request):
 @login_required
 def favorites(request):
     """Retrieve favorites."""
-    if request.method == "POST":
+    if request.method == "POST":  # le formulaire ne devrait pas se résoudre ici
         search_form = SearchProduct(request.POST or None)
         if search_form.is_valid():
             search_form = SearchProduct()
