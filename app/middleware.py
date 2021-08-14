ade25982328a4e1b09db32e5b5f3d67ef580cac3
Jerome-LorD <jerome-divry@laposte.net>
2021-08-14 17:10:08 +0200

    modifs html & add middleware.py

---
+++ app/middleware.py
@@ -0,0 +1,21 @@
+from purapps.purbeurre.forms import SearchProduct
+from django.shortcuts import redirect
+
+
+class SearchMiddleware:
+    """SearchMiddleware class."""
+
+    def __init__(self, get_response):
+        """Init."""
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """Call."""
+        search_form = SearchProduct()
+
+        if request.method == "POST" and request.POST.get("product_name"):
+            product_name = request.POST.get("product_name")
+            return redirect("results", product_name=product_name)
+
+        request.search_form = search_form
+        return self.get_response(request)
