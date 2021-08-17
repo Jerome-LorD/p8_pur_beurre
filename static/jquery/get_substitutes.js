3be8af37437c0a6d4a8652d8c13c620a1505c58b
Jerome-LorD <jerome-divry@laposte.net>
2021-08-17 17:32:34 +0200

    refactoring

--- static/jquery/get_substitutes.js
+++ static/jquery/get_substitutes.js
@@ -9,12 +9,11 @@ var get_substitutes = function () {
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
             type: 'POST',
-            url: '/ajax/',
+            url: ajax_url,
             data: JSON.stringify({ "products": selected_products, "user_id": '{{user.id}}', "origin_product": '{{origin_prod_name}}' }),
             csrfmiddlewaretoken: '{{ csrf_token }}',
 
         });
         e.stopImmediatePropagation();
-        /*return;*/
     });
 };
