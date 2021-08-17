f94bd6b88e50f5b02d4922bc427a96042d4dee39
Jerome-LorD <jerome-divry@laposte.net>
2021-08-17 14:56:44 +0200

    rm html files

--- purapps/purbeurre/models.py
+++ purapps/purbeurre/models.py
@@ -3,7 +3,6 @@
 from django.db import models
 from django.db.models import Count
 from django.conf import settings
-from django.urls import reverse
 
 
 class Nutriscore(models.Model):
@@ -33,8 +32,9 @@ class Category(models.Model):
 class Product(models.Model):
     """Create product table."""
 
-    name = models.CharField(max_length=250, unique=True, default=False, blank=True)
-    brand = models.CharField(max_length=180, default=False, blank=True)
+    name = models.CharField(max_length=150, unique=True, default=False, blank=True)
+    brand = models.CharField(max_length=150, default=False, blank=True)
+    stores = models.CharField(max_length=150, default=False, blank=True)
     image = models.URLField(max_length=255, default=False, blank=True, null=True)
     nutriments = models.JSONField()
     url = models.CharField(max_length=255, unique=True, default=False, blank=True)
@@ -43,8 +43,8 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         """Return str representation."""
-        return f"{self.id} - {self.name} - {self.brand} - {self.url} - {self.nutriscore}\
- - {self.image} - {self.nutriments}"
+        return f"{self.id} - {self.name} - {self.brand} - {self.stores} - {self.url}\
+ - {self.nutriscore} - {self.image} - {self.nutriments}"
 
     @classmethod
     def find_substitute(cls, product_id):
@@ -73,12 +73,17 @@ class Product(models.Model):
 
                 offset += 1
                 if data[0].nutriscore.type > "b":
-                    offset_limit = 4
-                offset_limit = 3
+                    offset_limit = 3
+                offset_limit = 2
                 if offset == offset_limit and not substitute:
                     substitute = data
             return substitute
         return data
+
+    @classmethod
+    def retrieve_substitute(cls):
+        """Retrieve substitute."""
+        return Substitutes.objects.values("product__name", "reference__name", "user_id")
 
 
 class Substitutes(models.Model):
