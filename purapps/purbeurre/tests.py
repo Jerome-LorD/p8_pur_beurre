404d3f933543d3d7d494a621b893106472c48014
Jerome-LorD <jerome-divry@laposte.net>
2021-08-27 09:54:03 +0200

    refactoring & adding some tests

--- purapps/purbeurre/tests.py
+++ purapps/purbeurre/tests.py
@@ -1,32 +1,24 @@
 """Tests purbeurre models module."""
 
-import unittest
-from purapps.purbeurre.models import Category, Product, Nutriscore, Substitutes
-from django.test import TestCase, RequestFactory, SimpleTestCase
-from purapps.purbeurre.views import favorites, product_details
 
-# from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model
+from django.db.models.expressions import Exists
+from django.test import Client
+from django.test import TestCase, RequestFactory
+from django.urls import resolve, reverse
+from purapps.purbeurre.views import favorites, product_details, index
+from purapps.purbeurre.models import Category, Product, Nutriscore, Substitutes
+
 
 User = get_user_model()
 
 
-class FillDbTestCase(unittest.TestCase):
+class FillDbTestCase(TestCase):
     """FindSubstitutesTestCase class."""
 
-    @classmethod
-    def setUpClass(cls):
-        """Make Setup."""
-        if cls is not FillDbTestCase and cls.setUp is not FillDbTestCase.setUp:
-            orig_setUp = cls.setUp
-
-            def setUpOverride(self, *args, **kwargs):
-                FillDbTestCase.setUp(self)
-                return orig_setUp(self, *args, **kwargs)
-
-            cls.setUp = setUpOverride
-
     def setUp(self):
+        """Make Setup."""
         products = [
             {
                 "product_name_fr": "Chocolat bio",
@@ -65,6 +57,9 @@ class FillDbTestCase(unittest.TestCase):
                 ),
             },
         ]
+
+        password = make_password("poufpouf")
+        self.user, _ = User.objects.get_or_create(username="bob", password=password)
 
         for product in products:
 
@@ -105,7 +100,9 @@ class FillDbTestCase(unittest.TestCase):
 
                     prod = Product.objects.get(name=product["product_name_fr"])
 
-                    category = Category.objects.filter(name=categorie).values("id")[0]
+                    category = (
+                        Category.objects.filter(name=categorie).values("id").first()
+                    )
 
                     prod.categories.add(category.get("id"))
 
@@ -129,69 +126,120 @@ class FindSubstitutesTestCase(FillDbTestCase):
             self.assertEqual(substit.name, "Chocolat noir sans sucres")
 
 
-class FavorisTestCase(FillDbTestCase):
+class FavoritesTestCase(FillDbTestCase):
     """FavorisTestCase class."""
 
     def setUp(self):
         """Make Setup."""
+        super().setUp()
 
-        self.factory = RequestFactory()
-        request = self.factory.get("/favorites")
-        self.user = User.objects.create_user(
-            username="bob", email="bob@bebo.com", password="poufpouf"
+        self.client = Client()
+
+        product = Product.objects.get(name="Chocolat bio")
+        substitute = Product.objects.get(name="Chocolat noir sans sucres")
+        Substitutes.objects.create(
+            reference=product, product=substitute, user=self.user
         )
-        products = {"product_id": 1, "ref_product_id": 2}
-        ref_product_id = products["ref_product_id"]
-        ref_product = Product.objects.get(pk=ref_product_id)
 
-        for item_id in products:
-            if Substitutes.objects.filter(
-                product_id=item_id,
-                reference_id=ref_product.id,
-                user_id=request.user.id,
-            ).exists():
-                pass
+    def test_favorites_status_code_200(self):
+        """Test favoris status_code 200."""
+        self.client.force_login(self.user)
+        response = self.client.get("/favorites/")
+        self.assertEqual(response.status_code, 200)
 
-            else:
-                substitute = Substitutes(
-                    product_id=item_id,
-                    reference_id=ref_product.id,
-                    user_id=request.user.id,
-                )
-                substitute.save()
+    def test_favorites_per_user(self):
+        """Test favoris per user."""
+        self.client.force_login(self.user)
+        res = Substitutes.objects.filter(user=self.user).first()
 
+        self.assertEqual(res.product.name, "Chocolat noir sans sucres")
 
-def test_favoris_status_code_200(self):
-    """Test favoris status_code 200."""
-    request = self.factory.get("/favorites")
-    request.user = self.user
-    response = favorites(request)
-    self.assertEqual(response.status_code, 200)
+    def test_favorites_page(self):
+        """Test favorites page."""
+        self.client.force_login(self.user)
+        response = self.client.get("/favorites/")
+        products = response.context["products"]
 
-
-def test_favoris_per_user(self):
-    """Test favoris per user."""
-    request = self.factory.get("/favorites")
-    request.user = self.user
-    breakpoint()
-    res = Substitutes.objects.filter(user=request.user)
-    self.assertEqual(res.name, "Chocolat noir sans sucres")
+        self.assertIn(1, [product.id for product in products])
 
 
 class ProductDetailsTestCase(FillDbTestCase):
-    """FavorisTestCase class."""
+    """ProductDetailsTestCase class."""
 
     def setUp(self):
         """Make Setup."""
-        self.factory = RequestFactory()
+        super().setUp()
+        self.client = Client()
         self.product = Product.objects.get(name="Chocolat bio")
 
     def test_product_details_status_code_200(self):
         """Test product status_code 200."""
-        request = self.factory.get("/product/Chocolat bio/")
-        response = product_details(request, "Chocolat bio")
+        response = self.client.get("/product/Chocolat bio/")
         self.assertEqual(response.status_code, 200)
 
     def test_product_details(self):
         """Test product."""
         self.assertEqual(self.product.name, "Chocolat bio")
+
+
+class IndexTestCase(TestCase):
+    """IndexTestCase class."""
+
+    def setUp(self):
+        """Make Setup."""
+        self.client = Client()
+
+    def test_index_status_code_200(self):
+        """Test index status_code 200."""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+class UrlsTestCase(TestCase):
+    """UrlsTestCase class."""
+
+    def test_urls(self):
+        """Test Urls."""
+        url = reverse("results", args=["Coca"])
+        self.assertEqual(url, "/results/Coca/")
+
+        url = reverse("product", args=["Coca-cola"])
+        self.assertEqual(url, "/product/Coca-cola/")
+
+
+class ProductModelTestCase(FillDbTestCase):
+    """ProductModelTestCase class."""
+
+    def setUp(self):
+        """Make Setup."""
+        super().setUp()
+
+    def test_name_label(self):
+        """Test name label."""
+        product = Product.objects.get(name="Chocolat bio")
+        field_label = product._meta.get_field("name").verbose_name
+        self.assertEquals(field_label, "name")
+
+    def test_name_max_length(self):
+        """Test name max_length 240."""
+        product = Product.objects.get(name="Chocolat bio")
+        max_length = product._meta.get_field("name").max_length
+        self.assertEquals(max_length, 240)
+
+
+class ResultsTestCase(FillDbTestCase):
+    """ResultsTestCase class."""
+
+    def setUp(self):
+        """Make Setup."""
+        super().setUp()
+
+    def test_results_status_code_200(self):
+        """Test results status_code 200."""
+        response = self.client.get("/results/Chocolat bio/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_results_product_is_none(self):
+        """Test results if product is None."""
+        self.product = Product.objects.filter(name__iregex=r"^%s$" % "bretelle").first()
+        self.assertIsNone(self.product, msg=None)
